47b1676e4ec16906bdaeaa3252d33b8b4c8f9355
SriramAnanth2912 <sriramananth2912@gmail.com>
2024-02-22 15:30:35 +0530

    created a pdf viewer

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+import PDFViewer from './PdfViewer';
+// need to include local file accessibility support
+const App = () => {
+  return (
+    <div>
+      <PDFViewer />
+    </div>
+  );
+};
+
+export default App;
