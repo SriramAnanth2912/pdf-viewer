babd9b9e1286966319a8e23c52c1393c6498d00d
Sriram Ananth <149565123+SriramAnanth2912@users.noreply.github.com>
2024-12-23 16:58:14 +0530

    reg commit

--- src/PdfViewer.jsx
+++ src/PdfViewer.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 
